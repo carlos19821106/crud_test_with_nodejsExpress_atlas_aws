dc8b6b50048127dcd3d833c5f1ab5f630e573f2b
Carlos Ma <wandog@gmail.com>
2022-10-26 20:56:07 +0800

    crud_test first commit

---
+++ routes/posts.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const Post = require('../models/Posts');
+
+//查询数据库所有数据
+router.get('/', async (req, res) => {
+    try {
+        const finePosts = await Post.find()
+        res.json(finePosts)
+    } catch (err) {
+        res.json({ message: err })
+    }
+});
+
+
+//add document
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        connect: req.body.connect
+    })
+    try {
+        const savePost = await post.save()
+        res.json(savePost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+});
+
+
+//find doc by ID
+router.get('/:postId', async (req, res) => {
+    try {
+        const findPost = await Post.findById(req.params.postId)
+        res.json(findPost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+});
+//del documents
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removePost = await Post.remove({ _id: req.params.postId })
+        res.json(removePost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+});
+//modify documents
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatePost = await Post.updateOne({ _id: req.params.postId }, { $set: { title: req.body.title ,connect:req.body.connect} } )
+        res.json(updatePost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+});
+
+module.exports = router
+
+
